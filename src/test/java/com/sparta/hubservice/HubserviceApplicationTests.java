6d54f4940f2cedd71897e7ecb9fc6c2d6e722908
fleurnari <fleur_nr@naver.com>
2025-11-06 11:32:33 +0900

    Initial commit

---
+++ src/test/java/com/sparta/hubservice/HubserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.hubservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
