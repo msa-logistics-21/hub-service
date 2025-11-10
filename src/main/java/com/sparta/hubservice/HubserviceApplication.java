572e9b8ad76b40416e8c09b58722e7a3b98313a5
fleurnari <fleur_nr@naver.com>
2025-11-10 18:27:34 +0900

    fix(hub) : 타 서비스 호출을 위한 설정 중(임시 커밋)

--- src/main/java/com/sparta/hubservice/HubserviceApplication.java
+++ src/main/java/com/sparta/hubservice/HubserviceApplication.java
@@ -2,8 +2,10 @@ package com.sparta.hubservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class HubserviceApplication {
 
     public static void main(String[] args) {
