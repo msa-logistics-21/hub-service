43a26fc173ba5c050a1c63ae87a6a3498573ab8a
SeongYeon Han <cicle1999@gmail.com>
2025-11-11 23:06:37 +0900

    refactor(hub-path): 허브 초기화, 허브경로 초기화 순서 지정

--- src/main/java/com/sparta/hubservice/global/init/HubInitializer.java
+++ src/main/java/com/sparta/hubservice/global/init/HubInitializer.java
@@ -6,12 +6,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Slf4j
 @Component
+@Order(1)
 @RequiredArgsConstructor
 public class HubInitializer implements ApplicationRunner {
 
--- src/main/java/com/sparta/hubservice/global/init/HubPathInitializer.java
+++ src/main/java/com/sparta/hubservice/global/init/HubPathInitializer.java
@@ -10,12 +10,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Slf4j
 @Component
+@Order(2)
 @RequiredArgsConstructor
 public class HubPathInitializer implements ApplicationRunner {
 
